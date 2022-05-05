d7b9e0e5458af8b2db646f669dd030cabe00bbaa
Preethi.S <Preethi.S@EXIND3020-01.allfon.in>
2022-05-05 16:27:45 +0530

    committed

---
+++ Credit-Card-Service/src/main/java/com/training/services/CreditCard.java
@@ -0,0 +1,45 @@
+package com.training.services;
+
+public class CreditCard {
+	
+	private long cardNumber;
+	private String cardHolderName;
+	private double amountDue;
+	
+	public CreditCard() {
+		super();
+	}
+
+	public CreditCard(long cardNumber, String cardHolderName, double amountDue) {
+		super();
+		this.cardNumber = cardNumber;
+		this.cardHolderName = cardHolderName;
+		this.amountDue = amountDue;
+	}
+
+	public long getCardNumber() {
+		return cardNumber;
+	}
+
+	public void setCardNumber(long cardNumber) {
+		this.cardNumber = cardNumber;
+	}
+
+	public String getCardHolderName() {
+		return cardHolderName;
+	}
+
+	public void setCardHolderName(String cardHolderName) {
+		this.cardHolderName = cardHolderName;
+	}
+
+	public double getAmountDue() {
+		return amountDue;
+	}
+
+	public void setAmountDue(double amountDue) {
+		this.amountDue = amountDue;
+	}
+	
+	
+}
---
+++ Credit-Card-Service/src/main/java/com/training/services/CreditCardService.java
@@ -0,0 +1,33 @@
+package com.training.services;
+
+public class CreditCardService {
+	
+	private Object[] arraySize;
+	
+	private boolean cardNumber(Object object) {
+		
+		return false;
+	}
+
+
+	public boolean addCard(boolean card1) {
+		
+		
+		long cardNumber;
+		if(cardNumber(arraySize[12]))  {
+			
+			return card1;
+		
+		}
+		return false;
+		
+	}
+	
+	
+	public CreditCard[] getCards(Object card) {
+		
+		System.out.println("CreditCard.getcardNumber");
+		
+		return getCards. getcardNumber();
+		}
+}
