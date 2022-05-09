1fd8af287ee20a53ca935a9060e98c72637e8558
Preethi.S <Preethi.S@EXIND3020-01.allfon.in>
2022-05-09 12:16:14 +0530

    committed

--- spring-quick-start/src/main/java/com/example/demo/SpringQuickStartApplication.java
+++ spring-quick-start/src/main/java/com/example/demo/SpringQuickStartApplication.java
@@ -4,6 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+import org.training.model.Author;
+import org.training.model.Book;
 
 import com.example.demo.model.Bill;
 import com.example.demo.model.CustomerList;
@@ -12,6 +18,7 @@ import com.example.demo.model.Student;
 import com.example.demo.model.Teacher;
 
 @SpringBootApplication
+@ComponentScan(basePackages = { "org.training", "com.example"})
 public class SpringQuickStartApplication {
 
 	public static void trial(String[] args) {
@@ -49,24 +56,34 @@ public class SpringQuickStartApplication {
 	public static void main(String[] args) {
 		
 		ApplicationContext ctx = SpringApplication.run(SpringQuickStartApplication.class, args);
-
+		
 		Invoice inv = ctx.getBean(Invoice.class);
 		System.out.println(inv);
 		
 		Bill bill = ctx.getBean(Bill.class);
-		
 		System.out.println("Customer:"+bill.getCustomer());
 		System.out.println("Product:"+bill.getProduct());
 		
 		CustomerList list = ctx.getBean(CustomerList.class);
-		
-		
-		System.out.println("======List======");
+		System.out.println("======Customer List======");
 		list.getCustList().forEach(System.out::println);
+		
+		System.out.println(ctx.getBean(Book.class));
+		System.out.println("author:" +ctx.getBean(Author.class));		
 	}
+		
+		@Bean 
+		public Author prawin() {
+			return new Author(501, "PrawinKumar");
+		}
+		
+		@Bean
+		@Primary
+		public Author kawin() {
+			return new Author(502, "KawinKumar");	
+		}
+
 }
-
-
 
 
 
