a0f1413ad63c1392c6de539545f30407e86c0fe5
Preethi.S <Preethi.S@EXIND3020-01.allfon.in>
2022-07-05 10:40:07 +0530

    bonus added

--- Task3/src/com/employee/service/EmployeeService.java
+++ Task3/src/com/employee/service/EmployeeService.java
@@ -1,64 +1,12 @@
-package com.BonusCaluculation;
-import java.util.*;
+package com.employee.service;
+
+import com.employee.task.Employee;
+
 public class EmployeeService {
-	private long salary;
-	private double bonus;
 	
-	
-	
-	public EmployeeService(long salary) {
-	super();
-	this.salary = salary;
-	}
-
-	
-
-	public EmployeeService(double bonus) {
-		super();
-		this.bonus = bonus;
-	}
-
-
-
-	public long getSalary() {
-		return salary;
-	}
-
-
-	public void setSalary(long salary) {
-		this.salary = salary;
-	}
-
-
-	
-	public double getBonus() {
-		return bonus;
-	}
-
-
-
-	public void setBonus(double bonus) {
-		this.bonus = bonus;
-	}
-
-
-
-	public static void main(String[] args) {
-		
-		 salary sal = new salary();
-		   
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the 33% bonus of the total Salary: "); 
-		long sal= nextLong();
-		sal.setsalary(sal);
-		System.out.println("Salary = " +sal.getsalary());
-		sc.close();
-		
-		
-		
-		
-	}
-	
-	
+	 public double calculateBonus(Employee sushmi) {
+		 double bonus= sushmi.getEmpSalary();
+		 return 0.33*bonus;
+	 }
 
 }
